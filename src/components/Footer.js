9263d5e5fb2df3bf68b65014f411d1c069f421b7
Lennox Otieno Lumumba <lennox.lumumba@student.moringaschool.com>
2023-04-07 22:41:43 +0300

    Changes made

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+const Footer = () => {
+    const year = new Date().getFullYear();
+  
+    return <footer>{`Copyright © NG-CDF Bursary ${year}`}</footer>;
+  };
+  
+  export default Footer;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import { BrowserRouter as Router } from "react-router-dom";
 import './index.css';
 import App from './components/App';
 import './App.css';
+import Footer from './components/Footer';
+import SocialFollow from './components/SocialFollow';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
     <App />
+    <SocialFollow />
+    <Footer />
   </Router>
 );
